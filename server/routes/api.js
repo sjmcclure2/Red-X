959c4cba22c5118a0706b987158f8800a7f9a6bd
Tyler Eggert <tyler.a.eggert@gmail.com>
2022-04-18 16:52:44 -0500

    Refactor routes into getTable

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,40 +3,24 @@ const knex = require('knex')(require('./../knexfile.js')[process.env.NODE_ENV]);
 
 const { BASE_URL } = require('../index')
 
-const categories = require('./categories');
-const project_owners = require('./project_owners');
-const projects = require('./projects');
-const requests = require('./requests');
-const requests_projects_join = require('./requests_projects_join');
-const users = require('./users');
-const users_projects_join = require('./users_projects_join');
+const getTable = require('./getTable');
 
 const router = express.Router();
 
 const tables = {};
 
-knex('pg_catalog.pg_tables')
-.select('tablename')
-.where({schemaname:'public'})
-.where('tablename', 'NOT ILIKE', 'knex%')
-.then(data =>
-  data.map(el => el.tablename)
-  .sort()
-  .forEach(el => {
-    tables[el] = `${BASE_URL}/${el}`
-  })
-);
+[ 'categories',
+  'project_owners',
+  'projects',
+  'requests'
+].forEach(el => {
+  tables[el] = `${BASE_URL}/${el}`
+});
 
 router.get('/', (req, res) => {
   res.json(tables);
 });
 
-router.use('/categories', categories);
-router.use('/project_owners', project_owners);
-router.use('/projects', projects);
-router.use('/requests', requests);
-router.use('/requests_projects_join', requests_projects_join);
-router.use('/users_projects_join', users_projects_join);
-router.use('/users', users);
+router.use([ '/categories', '/project_owners', '/projects', '/requests' ], getTable);
 
 module.exports = router;
--- server/routes/getTable.js
+++ server/routes/getTable.js
@@ -3,8 +3,8 @@ const knex = require('knex')(require('./../knexfile.js')[process.env.NODE_ENV]);
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  knex('project_owners')
+router.get('/', (req, res) => { 
+  knex(req.baseUrl.slice(5))
     // .where('description', '<>', '')
     .limit(req.query.limit ?? 20)
     .offset(req.query.offset ?? 0)
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const id = Number(req.params.id);
   if (Number.isInteger(id) && id > 0 && id < 2 ** 31) {
-    knex('project_owners')
+    knex(req.baseUrl.slice(5))
       .where('id', id)
       .then(data => res.json(data))
       .catch(err => console.error(err));
