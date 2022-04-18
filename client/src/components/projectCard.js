77b198a2d09c407c865eff2d1806a5831182c494
sjmcclure2 <sjmcclure2@gmail.com>
2022-04-18 13:16:32 -0500

    Co-authored-by: Mercedes Porter <MercedesPorter@users.noreply.github.com> Co-authored-by: JHammer494 <JHammer494@users.noreply.github.com> Co-authored-by: roninlul <roninlul@users.noreply.github.com>

--- client/src/components/projectCard.js
+++ client/src/components/projectCard.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 
-function ProjectCard({ project }) {
+function ProjectCard() {
+  const {id} = useParams();
+  const [project, setProject] = useState([]);
+  const [url, setUrl] = useState('http://localhost:8080/api');
 
+  useEffect(() => {
+    fetchData(url + `/projects/${id}`)
+    .then(data => {
+      setProject(data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },[]);
+
+  console.log(project);
   return(
     <div>
-      <p>{project.name}</p>
-      <p>{project.repo_owner}</p>
+      <p>{project?.[0]?.id}</p>
+      <p>{project?.[0]?.name}</p>
+      <p>{project?.[0]?.last_activity_at}</p>
+      <p>{project?.[0]?.status}</p>
     </div>
   )
 }
 
-export default ProjectCard
+async function fetchData(url) {
+  const response = await fetch(url)
+  let data = await response.json();
+  return data
+}
+
+export default ProjectCard;
